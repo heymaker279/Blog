a7280ee412f98e2ec3e4725b9c79cb142d294f69
Ильяс Валеев <ilyasvaleev@MacBook-Air-Ilas.local>
2024-04-13 08:12:36 +0500

    add templates and static

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404, HttpResponse
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    # return HttpResponse(posts)
+    return render(request, 'main/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    # return HttpResponse(post)
+    return render(request, 'main/post/detail.html', {'post': post})
